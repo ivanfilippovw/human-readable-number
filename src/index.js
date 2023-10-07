0f9fb17034db2f776cd3a015149e8c1cd62dfeb4
Ivan <ivanfilippovw@gmail.com>
2023-10-07 13:58:54 +0400

    feat: add base script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function toReadable (number) {
-  
+// module.exports =
+function toReadable (number) {
+    const numberToString = number.toString();
+    let result;
+
+    if (numberToString === '1') {
+      result = numberToString.replace('1', 'one')
+    } else if (numberToString === '2') {
+      result = numberToString.replace('2', 'two')
+    } else {
+      result = 'oops'
+    }
+
+    return result;
 }
+
+console.log(toReadable(1));
